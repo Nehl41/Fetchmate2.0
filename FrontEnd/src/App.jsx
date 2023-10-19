ce4991e0a4c54c7a14eabd05622a21d891f5870c
swayam101 <swayamprajapat21@gmail.com>
2023-10-19 14:09:52 +0530

    initial frontend commit

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>Nehal's React App On Vite</div>
+    </>
+  )
+}
+
+export default App
