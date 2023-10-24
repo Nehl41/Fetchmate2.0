9bdb44312d7caa26975b97a59c0ed7d3753b2e5a
swayam101 <swayamprajapat21@gmail.com>
2023-10-24 18:44:08 +0530

    product slider modified

--- FrontEnd/src/components/LandingSlider.jsx
+++ FrontEnd/src/components/LandingSlider.jsx
@@ -24,27 +24,29 @@ const LandingSlider = () => {
   })
 
   const carouselPrev=()=>{
-    let width=box.clientWidth
+    let width=box.offsetWidth
     box.scrollLeft=box.scrollLeft-width;
     console.log(`Width:${width}`);
     console.log(`Scroll Left:${box.scrollLeft}`);
   }
 
   const carouselNext=()=>{
-    let width=box.clientWidth
+    let width=box.offsetWidth
     box.scrollLeft=box.scrollLeft+width;
     console.log(`Width:${width}`);
     console.log(`Scroll Left:${box.scrollLeft}`);
   }
 
   return (
-    <div className="row-span-9 flex items-center justify-around">
-    <AiOutlineLeft style={{color:"gray"}} className='text-5xl' onClick={carouselPrev}/>
+    <div className="flex items-center justify-around">
+    <AiOutlineLeft style={{color:"gray"}} className='left-caros text-2xl' onClick={carouselPrev}/>
    <div id='carousel-container' className='flex carousel-container scroll-smooth w-full overflow-x-hidden'>
     {products.map(({name,price,weight,imageLink})=>(<ProductCard name={name} price={price} weight={weight} image={ProductImage}/>))}
    </div>
-<AiOutlineRight style={{color:"gray"}} className='text-5xl' onClick={carouselNext}/>
+<AiOutlineRight style={{color:"gray"}} className='right-caros text-5xl' onClick={carouselNext}/>
     </div>
+    
+   
   )
 }
 
