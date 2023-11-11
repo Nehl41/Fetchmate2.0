0594d6682b8d0791980cc40e68e8ae5c6f3d0e74
swayam101 <swayamprajapat21@gmail.com>
2023-11-11 15:20:39 +0530

    community page added

--- Back-End/routers/blog.js
+++ Back-End/routers/blog.js
@@ -1,11 +1,27 @@
 const router=require('express').Router()
-const {addBlog,getCloudinarySignature}=require('../controllers/blog')
+const {addBlog}=require('../controllers/blog')
 
-// router.get('/',)
+const cloudinary = require("cloudinary").v2;
+const multer=require('multer')
+const {CloudinaryStorage}=require('multer-storage-cloudinary')
 
-// router.get('/:id/blogs',)
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+    secure: true,
+  });
+  
+  const storage=new CloudinaryStorage({
+    cloudinary,
+    params:{
+      folder:'myFolder'
+    }
+  })
+  
+  const upload=multer({storage})
 
-router.post('/',addBlog);
+router.post('/',upload.single('image'),addBlog);
 
 
 module.exports=router
