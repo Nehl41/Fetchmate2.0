5cabecf34cb14a056a7190bdf1622f1c9a57333a
nehl41 <nehu.np20@gmail.com>
2023-11-18 16:34:39 +0530

    user Profile added

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -15,6 +15,7 @@ import Error404 from './pages/Error404/Error404.jsx'
 import Shop from './pages/Shop/Shop.jsx'
 import Login from './components/Login.jsx'
 import SignUp from './components/SignUp.jsx'
+import Profile from './components/UserProfile.jsx'
 
 const router = createBrowserRouter([
   {
@@ -31,6 +32,9 @@ const router = createBrowserRouter([
   },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <SignUp /> },
+  {
+    path: "/userprofile" , element: <Profile/>
+  },
   {
     path: "/upload",
     element: <UploadBlog />,
